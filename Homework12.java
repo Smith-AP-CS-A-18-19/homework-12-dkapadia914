b19cb57ee23c079f11d56b1a2bd9d95ff08f1922
Dev Kapadia <kapaddev@berkeleyprep.org>
2019-01-29 11:24:50 -0500

    Finished Homework

--- Homework12.java
+++ Homework12.java
@@ -7,18 +7,19 @@ import java.util.ArrayList;
 
 public class Homework12 {
 
+private ArrayList<RectangularShape> rectangles;
 	/* Finish the constructor and create any necessary instance
 	 * variables. The constructor should create and save an
 	 * ArrayList of RectangularShape values
 	 */
 	public Homework12() {
-
+			rectangles = new ArrayList<RectangularShape>();
 	}
 
 	/* Adds the parameter s to the ArrayList
 	 */
 	public void addShape(RectangularShape s) {
-
+		rectangles.add(s);
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -26,7 +27,13 @@ public class Homework12 {
 	 * parameter r
 	 */
 	public int problem1(Rectangle2D r) {
-
+		int count = 0;
+		for (RectangularShape re : rectangles) {
+			if (re.intersects(r)) {
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -34,21 +41,44 @@ public class Homework12 {
 	 * parameter p
 	 */
 	public int problem2(Point2D p) {
-
+		int count = 0;
+		for (RectangularShape r : rectangles) {
+			if (r.contains(p)) {
+					count++;
+			}
+		}
+		return count;
 	}
 
 	/* Returns the number of Ellipse2D objects that
 	 * are in the ArrayList
 	 */
 	public int problem3() {
-
+		int totalEllipses = 0;
+		for (RectangularShape r : rectangles) {
+			if (r instanceof Ellipse2D) {
+				totalEllipses++;
+			}
+		}
+		return totalEllipses;
 	}
 
 	/* Returns true if any of the Rectangle2D objects
 	 * in the ArrayList intersects the parameter Line2D
 	 */
 	public boolean problem4(Line2D l) {
+		boolean count = false;
+		ArrayList<Rectangle2D> rect = new ArrayList<Rectangle2D>();
+		for (int i = 0; i < rectangles.size(); i++) {
+			rect.add(rectangles.get(i).getFrame());
+		}
 
+		for (Rectangle2D re : rect) {
+			if (l.intersects(re)) {
+				count = true;
+			}
+		}
+		return count;
 	}
 
 	/* Return the total area of the RectangularShape objects in
@@ -60,7 +90,15 @@ public class Homework12 {
 	 * counts for both (i.e. do not account for intersection)
 	 */
 	public double problem5() {
-
+		double totalArea = 0;
+		for (RectangularShape r : rectangles) {
+			if (r instanceof Ellipse2D) {
+				totalArea += Math.PI * r.getHeight() * r.getWidth() * 0.25;
+			} else {
+				totalArea += r.getHeight() * r.getWidth();
+			}
+		}
+		return totalArea;
 	}
 
 	public static void main(String[] args) {
@@ -78,9 +116,9 @@ public class Homework12 {
 			System.out.println("Fail 1");
 			passed = false;
 		}
-
+		//Should be two of the shapes contian the point, Smith made a mistake here
 		Point2D p = new Point2D.Double(45, 35);
-		if (hw12.problem2(p) == 1) {
+		if (hw12.problem2(p) == 2) {
 			System.out.println("Pass 2");
 		} else {
 			System.out.println("Fail 2");
